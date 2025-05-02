603a37e9f0b35bf0c94463211ab8053f5e3828a4
manish <manishkr9547@gmail.com>
2025-05-02 20:10:05 +0530

    `Added Clerk React integration and updated navigation bar`

--- Hotel Booking/client/src/App.jsx
+++ Hotel Booking/client/src/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import { useLocation } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import AllRooms from "./pages/AllRooms";
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
@@ -8,6 +11,13 @@ const App = () => {
   return (
     <div>
       {!isOwnerPath && <Navbar />}
+      <div className="min-h-[70vh]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/rooms" element={<AllRooms />} />
+        </Routes>
+      </div>
+      <Footer />
     </div>
   );
 };
