53145ffce430200aa55847d5ae4f0f50ccf0a713
manish <manishkr9547@gmail.com>
2025-05-09 18:21:25 +0530

    Add hotel owner layout with Navbar and Sidebar components

--- Hotel Booking/client/src/App.jsx
+++ Hotel Booking/client/src/App.jsx
@@ -7,6 +7,7 @@ import AllRooms from "./pages/AllRooms";
 import RoomDetails from "./pages/RoomDetails";
 import MyBookings from "./pages/MyBookings";
 import HotelReg from "./components/HotelReg";
+import Layout from "./pages/hotelOwner/Layout";
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
@@ -14,13 +15,16 @@ const App = () => {
   return (
     <div>
       {!isOwnerPath && <Navbar />}
-     {false && <HotelReg />}
+      {false && <HotelReg />}
       <div className="min-h-[70vh]">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/rooms" element={<AllRooms />} />
           <Route path="/rooms/:id" element={<RoomDetails />} />
           <Route path="/my-bookings" element={<MyBookings />} />
+          <Route path="/owner" element={<Layout />}>
+          
+          </Route>
         </Routes>
       </div>
       <Footer />
